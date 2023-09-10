aa7c733e23bf03d1f4f896a7fe5b804d85f00807
al-kuznetsov <alkuznetsov@aol.com>
2023-09-10 18:06:24 +0500

    Initial commit

---
+++ src/main/java/com/tickaboo/server/TickabooServerApplication.java
@@ -0,0 +1,13 @@
+package com.tickaboo.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickabooServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickabooServerApplication.class, args);
+	}
+
+}
