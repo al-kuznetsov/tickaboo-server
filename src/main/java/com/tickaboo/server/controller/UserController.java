68f68000e4d80aa05e58ecbb403ebba341a29eab
Rinat <sibagatullin.rinat.r@gmail.com>
2023-09-29 18:32:11 +0500

    issue #8 Add RESTful endpoints for entity User, fix sequence in migration

---
+++ src/main/java/com/tickaboo/server/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.tickaboo.server.controller;
+
+import com.tickaboo.server.model.User;
+import com.tickaboo.server.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable Long id){
+        return repository.findById(id);
+    }
+
+    @PostMapping
+    public User create(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@RequestBody User newUser, @PathVariable Long id) {
+        return repository.findById(id).map(user -> {
+            user.setName(newUser.getName());
+            user.setFullName(newUser.getFullName());
+            user.setBio(newUser.getBio());
+            user.setEmail(newUser.getEmail());
+            user.setImage(newUser.getImage());
+            return repository.save(user);
+        }).orElseGet(() -> {
+            newUser.setId(id);
+            return repository.save(newUser);
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/tickaboo/server/model/User.java
@@ -0,0 +1,30 @@
+package com.tickaboo.server.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "user_account")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_account_seq")
+    @SequenceGenerator(name = "user_account_seq", sequenceName = "user_account_seq", allocationSize = 5)
+    private Long id;
+    private String name;
+    private String fullName;
+    private String bio;
+    private String email;
+    private String image;
+}
