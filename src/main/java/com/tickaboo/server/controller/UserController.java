4e7b5ca2273267666a65f4cfc16353aac1530d12
al-kuznetsov <alkuznetsov@aol.com>
2023-10-05 18:37:53 +0500

    issue-8 Add api version property

--- src/main/java/com/tickaboo/server/controller/UserController.java
+++ src/main/java/com/tickaboo/server/controller/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("${api.version.url}/users")
 public class UserController {
 
     private final UserRepository repository;
