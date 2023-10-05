4c2ba748c8a347d5ca65784c05a028023e409e22
al-kuznetsov <alkuznetsov@aol.com>
2023-10-05 18:30:49 +0500

    issue-8 Fix User entity implementation

--- src/main/java/com/tickaboo/server/model/User.java
+++ src/main/java/com/tickaboo/server/model/User.java
@@ -1,20 +1,21 @@
 package com.tickaboo.server.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
+import org.hibernate.annotations.NaturalId;
 
 @NoArgsConstructor
 @Getter
 @Setter
-@ToString
 @Entity
 @Table(name = "user_account")
 public class User {
@@ -22,9 +23,38 @@ public class User {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_account_seq")
     @SequenceGenerator(name = "user_account_seq", sequenceName = "user_account_seq", allocationSize = 5)
     private Long id;
+    @NaturalId
+    private String login;
     private String name;
     private String fullName;
     private String bio;
+    @Column(unique = true)
     private String email;
     private String image;
+
+    public User(String login) {
+        this.login = login;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User user)) return false;
+        return Objects.equals(getLogin(), user.getLogin());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLogin());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
