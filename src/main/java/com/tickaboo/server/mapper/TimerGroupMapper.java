dc0feeb2a0cbc2f7314d65dcd1765a0b137823e2
Rinat <sibagatullin.rinat.r@gmail.com>
2023-10-21 20:22:50 +0500

    Revert "issue-15 Add timer group API"
    This reverts commit 492bf62df27561145b4f75c152952d7ebf39306a.

--- src/main/java/com/tickaboo/server/mapper/TimerGroupMapper.java
+++ src/main/java/com/tickaboo/server/mapper/TimerGroupMapper.java
@@ -5,9 +5,12 @@ import com.tickaboo.server.model.TimerGroup;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(config = StandardMapperConfiguration.class)
+@Mapper(config = StandardMapperConfiguration.class,
+        uses = LocalDateTimeMapper.class)
 public interface TimerGroupMapper {
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "lastUpdatedAt", ignore = true)
     TimerGroup fromDto(TimerGroupDto timerGroupDto);
 
     TimerGroupDto toDto(TimerGroup timerGroup);
--- src/main/java/com/tickaboo/server/model/TimerGroup.java
+++ src/main/java/com/tickaboo/server/model/TimerGroup.java
@@ -1,6 +1,11 @@
 package com.tickaboo.server.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,7 +21,6 @@ public class TimerGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
     @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 5)
-
     private Long id;
     private String name;
 }
